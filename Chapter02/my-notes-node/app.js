3780401bc59f6e28546c89ed4bf8eb7447a69b3c
David Harillo Sánchez <dhs.seck@gmail.com>
2019-08-14 22:21:58 +0200

    Configure as node package

--- Chapter02/my-notes-node/app.js
+++ Chapter02/my-notes-node/app.js
@@ -2,10 +2,13 @@ console.log('Starting app...');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
 
+console.log(notes.add(9, -2));
+process.exit(0);
 const user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) => {
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, (err) => {
   if (err) {
     console.error('Unable to write into greetings.txt', err);
   }
