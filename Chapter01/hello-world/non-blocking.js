af1f2b0e69d93a0ff7a0f759e5ce5af8bed26bec
David Harillo Sánchez <dhs.seck@gmail.com>
2019-08-14 13:10:17 +0200

    Autofix chapter files

--- Chapter01/hello-world/non-blocking.js
+++ Chapter01/hello-world/non-blocking.js
@@ -1,14 +1,14 @@
-var getUser = require('getUser');
+const getUser = require('getUser');
 
 console.log('starting user1');
-getUser('123', function (user1) {
+getUser('123', (user1) => {
   console.log('user1', user1);
 });
 
 console.log('starting user2');
-getUser('321', function (user2) {
+getUser('321', (user2) => {
   console.log('user2', user2);
 });
 
-var sum = 1 + 2;
-console.log('The sum is ' + sum);
+const sum = 1 + 2;
+console.log(`The sum is ${sum}`);
