1cca90a0aa3bc96bf8498d97ba3979829406c5d6
David Harillo Sánchez <dhs.seck@gmail.com>
2019-08-17 00:14:46 +0200

    Check weather app API key requirement

--- Chapter05/weather-app/app.js
+++ Chapter05/weather-app/app.js
@@ -1,10 +1,18 @@
 const request = require('request');
 
 request({
-  url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
-  json: true
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  json: true,
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if (error) {
+    console.error('Unexpected error', error);
+  } else if (response.statusCode !== 200) {
+    console.warn('Rejected request', body);
+  } else if (body.status === 'REQUEST_DENIED') {
+    console.error(body.error_message);
+  } else {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
