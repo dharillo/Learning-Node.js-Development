fc7a131c665908dee82519bcda59b032e886a107
David Harillo Sánchez <dhs.seck@gmail.com>
2019-08-20 00:09:16 +0200

    Add partials for header and footer

--- Chapter08/my-node-web-server/index.js
+++ Chapter08/my-node-web-server/index.js
@@ -7,20 +7,31 @@ const SERVER_PORT = 3000;
 
 const app = express();
 
+hbs.registerPartials(join(__dirname, 'views', 'partials'));
 app.set('view engine', 'hbs');
 app.use(express.static(join(__dirname, 'public')));
 
+/**
+ * Gets the current year
+ *
+ * @returns {number} Current year
+ */
+function getCurrentYear() {
+  return new Date().getFullYear();
+}
+
 app
   .get('/', (_, res) => {
-    res.json({
-      name: 'David',
-      likes: ['Videogames', 'Computers'],
+    res.render('home.hbs', {
+      pageTitle: 'Home page',
+      currentYear: getCurrentYear(),
+      welcomeMessage: 'Welcome to the Learning Node.js Development homepage',
     });
   })
   .get('/about', (_, res) => {
     res.render('about.hbs', {
       pageTitle: 'About Page',
-      currentYear: new Date().getFullYear(),
+      currentYear: getCurrentYear(),
     });
   })
   .get('/bad', (_, res) => {
